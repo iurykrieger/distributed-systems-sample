6390baabc2af4fb24ddbf949ed1c278c280807ad
Iury Krieger <iury.krieger@gmail.com>
2023-09-23 18:02:38 -0300

    fix: remove unnecessary deps

--- apps/search-api/src/search.module.ts
+++ apps/search-api/src/search.module.ts
@@ -2,15 +2,9 @@ import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
-    EventEmitterModule.forRoot({
-      maxListeners: 9999999,
-      verboseMemoryLeak: false,
-      ignoreErrors: true 
-    }),
     ElasticsearchModule.register({
       node: process.env.ELASTIC_SEARCH_URL
     })
