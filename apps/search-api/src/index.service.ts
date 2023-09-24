eb0bf77fde23f95476fa8136c1c59a6dabdbcc8d
Iury Krieger <iury.krieger@gmail.com>
2023-09-23 22:54:22 -0300

    feat: full index endpoint

---
+++ apps/search-api/src/index.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { SearchService } from './search.service';
+import { createReadStream, readdirSync } from 'node:fs';
+import { join } from 'node:path';
+import * as es from 'event-stream'
+import { ProductDto } from './dto/product.dto';
+
+
+@Injectable()
+export class IndexService {
+    private readonly logger = new Logger(SearchService.name)
+    private readonly CHUNK_SIZE = 300
+    private readonly BASE_DIR = './data/products'
+
+    constructor(
+        private readonly searchService: SearchService
+    ) { }
+
+    async indexAll() {
+        const productsDir = readdirSync(this.BASE_DIR)
+
+        await Promise.all(productsDir.map(file =>
+            this.readProductsFileByChunks(`${this.BASE_DIR}/${file}`, products =>
+                this.searchService.index(products)
+            )
+        ))
+    }
+
+    private async readFileByLine(filePath: string, callback: (line: string) => Promise<void>): Promise<void> {
+        return new Promise(async (resolve, reject) => {
+            createReadStream(join(process.cwd(), filePath), { encoding: 'utf-8' })
+                .pipe(es.split())
+                .pipe(es.mapSync((line: string) => callback(line)))
+                .on('error', reject)
+                .on('end', resolve)
+        })
+    }
+
+    private async readProductsFileByChunks(filePath: string, callback: (chunk: ProductDto[]) => Promise<void>): Promise<void> {
+        let chunk: ProductDto[] = []
+        await this.readFileByLine(filePath, async (line: string) => {
+            const product = this.parseProduct(line)
+            chunk.push(product)
+
+            if (chunk.length % this.CHUNK_SIZE === 0) {
+                await callback(chunk)
+                chunk = []
+            }
+        })
+    }
+
+    private parseProduct(data: any): ProductDto {
+        if (!data) {
+            return
+        }
+
+        const product: ProductDto = JSON.parse(data)
+
+        product.price = Number.parseFloat(product.price?.toString())
+        product.oldPrice = Number.parseFloat(product.oldPrice?.toString())
+
+        return product
+    }
+}
--- apps/search-api/src/search.controller.ts
+++ apps/search-api/src/search.controller.ts
@@ -1,14 +1,23 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { ProductDto } from './dto/product.dto';
+import { IndexService } from './index.service';
 
 @Controller()
 export class SearchController {
-  constructor(private readonly searchService: SearchService) {}
+  constructor(
+    private readonly searchService: SearchService,
+    private readonly indexService: IndexService
+  ) {}
+
+  @Post('index/all')
+  indexAll() {
+    this.indexService.indexAll()
+  }
 
   @Post('index')
   index(@Body() product: ProductDto) {
-    return this.searchService.index(product)
+    return this.searchService.index([product])
   }
 
   @Get('products/:id')
