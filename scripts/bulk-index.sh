ad4ed5dd1ceac3ce6e327660c3c1bfe44e8bdee2
Iury Krieger <iury.krieger@gmail.com>
2023-10-14 16:16:02 -0300

    fix: increase ES limits

--- scripts/bulk-index.sh
+++ scripts/bulk-index.sh
@@ -7,5 +7,5 @@
 # for file in chunks/c-*; do cat $file | jq '{ id: .id, apikey: .apiKey, name: .name, price: .price, oldPrice: .oldPrice, description: .description }' | jq -s '.' > $file.json && rm $file; done
 
 # Send chunks
-ls data/products/chunks/*.json -A | xargs -d $'\n' -n 30 -P 100 \
-  bash -c 'for line in "$@"; do curl -s -X POST "http://10.152.183.27/index" -H "Content-type: application/json" -d @$line; done'
+ls data/products/chunks/*.json -A | xargs -d $'\n' -n 10 -P 50 \
+  bash -c 'for line in "$@"; do curl -s -X POST "http://10.152.183.27/index" -H "Content-type: application/json" -d @$line; done' > /dev/null
